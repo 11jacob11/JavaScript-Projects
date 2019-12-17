28e873c78fbee4b164314dee05f5fa7146d54b11
Jacob Seibel <jacobpieman99@gmail.com>
2019-12-16 16:15:43 -0800

    complete more work

---
+++ Basic JavaScript Projects/JS/Basic_JavaScript_8.js
@@ -0,0 +1,13 @@
+// var str1 = "Hel";
+// var str2 = "lo!";
+// var str3 = str1.concat(str2);
+// console.log(str3);
+
+var bigStr = "Hey there Stanly!";
+var name = bigStr.slice(10, 16);
+
+var num1 = 17;
+var numStr = num1.toString();
+
+var num2 = 1.1234567890;
+var num3 = num2.toPrecision(6);
