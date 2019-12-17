abc363ab5d80396f5fdef52c8e7123a3722e91f7
Jacob Seibel <jacobpieman99@gmail.com>
2019-12-16 19:06:19 -0800

    complete more work

---
+++ Basic JavaScript Projects/JS/Basic_JavaScript_10.js
@@ -0,0 +1,40 @@
+function Call_Loop() {
+    var num = 0;
+    while (num < 51) {
+        num++;
+    }
+}
+
+function for_Loop() {
+    var num = 0
+    for (var i = 0; i < 15; i++) {
+        num++;
+        console.log(num);
+    }
+}
+
+function array_function(typeNum) {
+    var array = ["sand", "gravel", "dirt", "concrete", "grass"];
+    document.getElementById("array").innerHTML = array[typeNum];
+}
+
+const Jacob1 = {
+    name: "Jacob S",
+    gender: "M",
+    hairColor: "Brown",
+}
+
+function constant_function() {
+    document.getElementById("constant").innerHTML = Jacob.name;
+}
+
+let ham = "tasty";
+
+let Jacob2 = {
+    name: "Jacob S",
+    gender: "M",
+    hairColor: "Brown",
+    speak: function() {
+        return "Hello there, my name is Jacob.";
+    }
+}
